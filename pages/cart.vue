2d9478be1751a01811e7e8f2ebc1d96b784ea94f
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2023-12-15 23:14:02 +0400

    update layout

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,31 +1,37 @@
 <template>
-  <div
-    class="grid h-full grid-cols-none gap-3 lg:grid-cols-3"
-    :class="{ 'relative grid grid-cols-none gap-3 lg:grid-cols-3': isCartValidLoading }"
-  >
-    <ClientOnly>
-      <template #fallback>
-        <div class="space-y-3 lg:col-span-2">
-          <CartCardSkeleton v-for="item in createArray(4)" :key="item" />
+  <NuxtLayout :name="cartStore.items ? 'default' : 'main'">
+    <div
+      class="grid h-full grid-cols-none gap-3 lg:grid-cols-3"
+      :class="{ 'relative grid grid-cols-none gap-3 lg:grid-cols-3': isCartValidLoading }"
+    >
+      <ClientOnly>
+        <template #fallback>
+          <div class="space-y-3 lg:col-span-2">
+            <CartCardSkeleton v-for="item in createArray(4)" :key="item" />
+          </div>
+
+          <CartSummarySkeleton />
+        </template>
+
+        <template v-if="cartStore.items">
+          <CartList class="lg:col-span-2" :items="cartStore.items" />
+          <CartSummary />
+        </template>
+
+        <div v-else class="col-span-3 flex h-full items-center justify-center">
+          <Result title="Корзина пуста 😳" />
         </div>
-
-        <CartSummarySkeleton />
-      </template>
-
-      <template v-if="cartStore.items">
-        <CartList class="lg:col-span-2" :items="cartStore.items" />
-        <CartSummary />
-      </template>
-
-      <div v-else class="col-span-3 flex h-full items-center justify-center">
-        <Result title="Корзина пуста 😳" />
-      </div>
-    </ClientOnly>
-  </div>
+      </ClientOnly>
+    </div>
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
 const cartStore = useCartStore();
 
-const isCartValidLoading = cartStore.isLoading || cartStore.items?.length;
+const isCartValidLoading = cartStore.isLoading || cartStore.items;
+
+definePageMeta({
+  layout: false,
+});
 </script>
