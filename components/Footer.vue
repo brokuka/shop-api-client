6fb48c61f7c3e79d2f95e58cc88bada1a914baf5
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2023-12-15 22:51:47 +0400

    changes

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- bg-background/75 backdrop-blur border-b border-gray-200 dark:border-gray-800 -->
   <footer class="z-[2] hidden border-t border-gray-200 dark:border-gray-800 md:block">
     <UContainer>
       <GithubButton />
