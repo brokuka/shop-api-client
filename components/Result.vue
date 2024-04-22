0144132811f8e6d0c9f432c1bf85af072838b5a0
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2024-04-22 16:55:00 +0400

    chore: update result component

--- components/Result.vue
+++ components/Result.vue
@@ -8,16 +8,19 @@ const slot = useSlots()
 </script>
 
 <template>
-  <div class="px-8 py-10">
-    <p class="my-2 text-center text-xl font-semibold">
-      {{ title }}
-    </p>
-    <p v-if="subTitle" class="text-center text-sm">
-      {{ subTitle }}
-    </p>
+  <div>
+    <div class="px-8 py-10 flex flex-col gap-4 items-center">
+      <p class="my-2 text-center text-xl font-semibold">
+        {{ title }}
+      </p>
 
-    <div v-if="slot.default" class="pt-4">
-      <slot />
+      <p v-if="subTitle" class="text-center text-sm">
+        {{ subTitle }}
+      </p>
+
+      <div v-if="slot.default">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
