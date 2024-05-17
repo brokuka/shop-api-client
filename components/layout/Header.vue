18b7cb7492d48dc1ebcc7491c7d7eaebb3929ed6
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2024-05-17 19:01:25 +0400

    chore: adding version dark text color

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -13,7 +13,7 @@ const cartStore = useCartStore()
 <template>
   <header class="z-[3] border-b border-gray-200 dark:border-gray-800 bg-white dark:bg-black">
     <UContainer class="flex h-16 items-center justify-between">
-      <ULink to="/" class="hover:text-primary p-4.5 text-lg font-medium group" active-class="text-primary [&>sup]:text-black">
+      <ULink to="/" class="hover:text-primary p-4.5 text-lg font-medium group" active-class="text-primary [&>sup]:text-black [&>sup]:dark:text-white">
         SHOP-API
         <sup class="text-primary">v{{ version }}</sup>
       </ULink>
